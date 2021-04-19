4179897571b9be17ae2885235bbc75540b9b36b8
Murillo R. Araujo <mr_araujo@icloud.com>
2021-04-19 17:20:05 -0300

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Murillo R. Araujo on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clima/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clima
+//
+//  Created by Murillo R. Araujo on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
